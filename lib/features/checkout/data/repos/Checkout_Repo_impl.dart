759dd2c25b215af5d14a6fc7e8b495a62bd2a144
MarwaAtef <marwaatef2001@gmail.com>
2025-02-03 22:41:20 +0200

    create Check out Repo Impl

---
+++ lib/features/checkout/data/repos/Checkout_Repo_impl.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment/core/errors/failure.dart';
+import 'package:payment/core/utils/stripe_service.dart';
+import 'package:payment/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:payment/features/checkout/data/repos/checkout_repo.dart';
+
+class CheckoutRepoImpl extends CheckoutRepo {
+  final StripeService stripeService = StripeService();
+  @override
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    try {
+      await stripeService.makePayment(
+          PaymentIntentInputModel: paymentIntentInputModel);
+      return right(null);
+    } catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+}
